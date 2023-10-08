ad90dd7e1794c39d293d73bf33242df3e1917d9a
scheeti <scheeti@NS099>
2023-10-08 17:20:46 +0530

    Add project files.

---
+++ CRUD_MVC/Controllers/EmployeeController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CRUD_MVC.Models;
+
+
+namespace CRUD_MVC.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        [HttpGet]
+        public ActionResult EmpOperations()
+        { 
+            return View();
+        }
+        [HttpPost]
+        public ActionResult EmpOperations(string B1,EmployeeModel E)
+        {
+            EmpOperations EOP = new EmpOperations();
+            if(B1.Equals("INSERT"))
+            {
+                int i = EOP.AddEmployee(E);
+                if (i == 1)
+                    ViewBag.Message = "Employee Added Successfully";
+                else
+                    ViewBag.Message = "Error Occurred";
+            }
+            else if(B1.Equals("UPDATE"))
+            {
+                int i = EOP.UpdateEmployee(E);
+                if (i == 1)
+                    ViewBag.Message = "Employee details Updated Successfully";
+                else
+                    ViewBag.Message = "Error Occurred";
+            }
+            else
+            {
+                int i = EOP.DeleteEmployee(E.Empid);
+                if (i == 1)
+                    ViewBag.Message = "Employee details Deleted Successfully";
+                else
+                    ViewBag.Message = "Error Occurred";
+            }
+            return View();
+        }
+    }
+}
---
+++ CRUD_MVC/Models/EmployeeModel.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+namespace CRUD_MVC.Models
+{
+    public class EmployeeModel
+    {
+        public int Empid { set; get; }
+        public string  Ename { set; get; }
+        public string  Designation { set; get; }
+        public double  Salary { set; get; }
+    }
+    public class EmpOperations
+    {
+        SqlConnection Con = new SqlConnection("Server = 40.76.121.111; user id = sa; password=NetScore@Integrations; Database=DotNetTraining");
+        SqlCommand Cmd;
+        public int AddEmployee(EmployeeModel Emp)
+        {
+            string Query = "Insert into Empdetails values(@P1,@P2,@P3,@P4)";
+            Cmd = new SqlCommand(Query, Con);
+            Cmd.Parameters.AddWithValue("@P1", Emp.Empid);
+            Cmd.Parameters.AddWithValue("@P2", Emp.Ename);
+            Cmd.Parameters.AddWithValue("@P3", Emp.Designation);
+            Cmd.Parameters.AddWithValue("@P4", Emp.Salary);
+            Con.Open();
+            int i = Cmd.ExecuteNonQuery();
+            Con.Close();
+            return i;
+        }
+        public int UpdateEmployee(EmployeeModel Emp)
+        {
+            string Query = "Update Empdetails set Ename=@P1,Designation=@P2,Salary=@P3 where Empid=@P4";
+            Cmd = new SqlCommand(Query, Con);
+            Cmd.Parameters.AddWithValue("@P4", Emp.Empid);
+            Cmd.Parameters.AddWithValue("@P1", Emp.Ename);
+            Cmd.Parameters.AddWithValue("@P2", Emp.Designation);
+            Cmd.Parameters.AddWithValue("@P3", Emp.Salary);
+            Con.Open();
+            int i = Cmd.ExecuteNonQuery();
+            Con.Close();
+            return i;
+        }
+        public int DeleteEmployee(int Id)
+        {
+            string Query = "Delete from Empdetails where Empid=@P1";
+            Cmd = new SqlCommand(Query, Con);
+            Cmd.Parameters.AddWithValue("@P1", Id);
+            Con.Open();
+            int i = Cmd.ExecuteNonQuery();
+            Con.Close();
+            return i;
+        }
+    }
+
+
+}
